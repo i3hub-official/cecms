1b1af7d5e84451b013994dc40427708556664b26
OgwoGP <digispowerllc@gmail.com>
2025-09-16 04:11:42 +0100

    Update index.ts

--- src/lib/server/db/index.ts
+++ src/lib/server/db/index.ts
@@ -1,10 +1,10 @@
-import { drizzle } from 'drizzle-orm/postgres-js';
-import postgres from 'postgres';
-import * as schema from './schema';
-import 'dotenv/config';
+import { drizzle } from "drizzle-orm/postgres-js";
+import postgres from "postgres";
+import * as schema from "./schema";
+import "dotenv/config";
 
-if (!process.env.DATABASE_URL) throw new Error('DATABASE_URL is not set');
+if (!process.env.cecms_DATABASE_URL) throw new Error("DATABASE_URL is not set");
 
-const client = postgres(process.env.DATABASE_URL);
+const client = postgres(process.env.cecms_DATABASE_URL);
 
 export const db = drizzle(client, { schema });
