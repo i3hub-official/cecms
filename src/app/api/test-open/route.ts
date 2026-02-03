1556630188ce9cd6ad70c2074311d871a2a3676b
DigispowerLLC <digispowerllc@gmail.com>
2026-02-03 22:21:53 +0100

    SD

--- src/app/api/test-open/route.ts
+++
@@ -1,6 +0,0 @@
-// src/app/api/test-open/route.ts
-import { NextResponse } from "next/server";
-
-export async function GET() {
-  return NextResponse.json({ success: true, message: "No auth here" });
-}
